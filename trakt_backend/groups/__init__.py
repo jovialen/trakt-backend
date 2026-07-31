207fe2d9ce57f8113838a73a0f40b54c357f584a
Nicolai Frigaard <46283121+jovialen@users.noreply.github.com>
2026-07-31 23:28:43 +0200

    feat: add feed and group relationship
    Add a many-to-many relationship between feeds and groups.

--- trakt_backend/groups/__init__.py
+++ trakt_backend/groups/__init__.py
@@ -1,3 +1,2 @@
 from .controller import router
 from .dto import FeedGroupCreate, FeedGroupPatch, FeedGroupUpdate
-from .model import FeedGroup, FeedGroupBase
--- trakt_backend/groups/model.py
+++ trakt_backend/groups/model.py
@@ -1,4 +1,11 @@
-from sqlmodel import Field, SQLModel
+from typing import TYPE_CHECKING
+
+from sqlmodel import Field, Relationship, SQLModel
+
+# Circular import here, but since its only because of type checking, this is an acceptable solution
+if TYPE_CHECKING:
+    from ..feeds import Feed
+from ..feed_group.model import FeedGroupLink
 
 
 class FeedGroupBase(SQLModel):
@@ -6,4 +13,8 @@ class FeedGroupBase(SQLModel):
 
 
 class FeedGroup(FeedGroupBase, table=True):
+    __tablename__ = "group"
+
     id: int = Field(default=None, primary_key=True)
+
+    feeds: list[Feed] = Relationship(back_populates="groups", link_model=FeedGroupLink)
