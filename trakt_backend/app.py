262543ad5639ac1f6d9b1b9d496746eacb34fe63
Nicolai Frigaard <46283121+jovialen@users.noreply.github.com>
2026-07-31 19:25:53 +0200

    7 app settings (#8)
    * feat: create app settings
    
    Create global app settings in settings.py. File contains both constants and .env files
    
    * chore: wire app settings
    
    Wire settings from settings.py into the application in app.py

--- trakt_backend/app.py
+++ trakt_backend/app.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from . import feed
+from . import feed, settings
 from .database import create_db_and_tables
 
 
@@ -12,5 +12,19 @@ async def lifespan(_app: FastAPI):
     yield
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    title=settings.APP_NAME,
+    description=settings.DESCRIPTION,
+    version=settings.VERSION,
+    lifespan=lifespan
+)
 app.include_router(feed.router)
+
+
+@app.get("/", tags=["App"])
+def app_info():
+    return {
+        "app_name": settings.APP_NAME,
+        "app_description": settings.DESCRIPTION,
+        "app_version": settings.VERSION,
+    }
